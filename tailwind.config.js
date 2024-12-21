ff9d782be5929625b5c687cb115a87e54114aaf8
Satrioagung10 <razorcallahan54@gmail.com>
2024-12-21 08:41:16 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  devServer: {  
+    hot: true  
+  },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
